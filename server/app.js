a7c68fdb8415bcc79847cea1f08274759dda97b4
Arnoldo Filadelfo Santos Angel <110629283+ArnoldoS04@users.noreply.github.com>
2026-01-06 17:19:17 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,9 @@ import routes from "./routes/index.routes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import https from "https";
-import fs from "fs";
 
 dotenv.config();
 
-const key = fs.readFileSync("./certs/infracod.local-key.pem");
-const cert = fs.readFileSync("./certs/infracod.local.pem");
 
 const app = express();
 app.use(express.json());
@@ -19,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://infracod.local", // Aquí debe ser HTTPS si frontend usa HTTPS
+    origin: "https://panel.infracod.com", // Aquí debe ser HTTPS si frontend usa HTTPS
     credentials: true,
   })
 );
@@ -28,5 +25,5 @@ app.use("/api", routes);
 
 // Crear servidor HTTPS
 https.createServer({ key, cert }, app).listen(process.env.PORT, () => {
-  console.log(`Server corriendo en https://infracod.local:${process.env.PORT}`);
+  console.log(`Server corriendo en https://panel.infracod.com:${process.env.PORT}`);
 });
