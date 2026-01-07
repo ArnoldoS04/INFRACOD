a127eb246b4f4d597fef1130fa88ea66c4aef332
Arnoldo Filadelfo Santos Angel <110629283+ArnoldoS04@users.noreply.github.com>
2026-01-06 21:59:57 -0600

    Remove port from authFetch base URL

--- client/src/components/AuthFecth.jsx
+++ client/src/components/AuthFecth.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 
 // Crea una instancia de Axios
 const authFetch = axios.create({
-  baseURL: "https://api.infracod.com:3000/api",
+  baseURL: "https://api.infracod.com/api",
   withCredentials: true, // importante para enviar la cookie del refreshToken
 });
 
@@ -32,7 +32,7 @@ authFetch.interceptors.response.use(
 
       try {
         const res = await axios.post(
-          "https://api.infracod.com:3000/api/auth/refresh",
+          "https://api.infracod.com/api/auth/refresh",
           null,
           {
             withCredentials: true,
